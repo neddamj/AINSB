a3aca5b85c27d5a474754cf77644652de88d4895
Jordan-Madden <jordanmadden285@gmail.com>
2021-01-13 19:24:50 -0500

    Testing realsense camera

--- src/depth_estimation/compute_depth.py
+++ src/depth_estimation/compute_depth.py
@@ -1,47 +1,13 @@
-import pyrealsense2 as rs
-
-try:
-    # Create a context object. This object owns the handles to all connected realsense devices
-    pipeline = rs.pipeline()
-    pipeline.start()
-
-    while True:
-        # This call waits until a new coherent set of frames is available on a device
-        # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
-        frames = pipeline.wait_for_frames()
-        depth = frames.get_depth_frame()
-        if not depth: continue
-
-        # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
-        coverage = [0]*64
-        for y in range(480):
-            for x in range(640):
-                dist = depth.get_distance(x, y)
-                if 0 < dist and dist < 1:
-                    coverage[x//10] += 1
-            
-            if y%20 is 19:
-                line = ""
-                for c in coverage:
-                    line += " .:nhBXWW"[c//25]
-                coverage = [0]*64
-                print(line)
-    exit(0)
-#except rs.error as e:
-#    # Method calls agaisnt librealsense objects may throw exceptions of type pylibrs.error
-#    print("pylibrs.error was thrown when calling %s(%s):\n", % (e.get_failed_function(), e.get_failed_args()))
-#    print("    %s\n", e.what())
-#    exit(1)
-except Exception as e:
-    print(e)
-    pass
-
 '''
+    Usage: python compute_depth.py
+'''
+
 import pyrealsense2 as rs
 import numpy as np
 import cv2
 
 # Configure depth and color streams
+print("[INFO] building and configuring the video pipeline...")
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
@@ -49,16 +15,20 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
+print("[INFO] starting video stream...")
 
 try:
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
+        depth = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
         if not depth_frame or not color_frame:
             continue
+
+        # Find the distance of an arbitraty point in the video frame
+        dist = depth.get_distance('''x, y''')
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
@@ -66,17 +36,27 @@ try:
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        
+        # Resize the images to known dimensions
+        cv2.resize(color_image, (224, 224))
+        cv2.resize(depth_colormap, (224, 224))
 
-        # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
+        '''# Stack both images horizontally
+        images = np.hstack((color_image, depth_colormap))'''
 
         # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+        cv2.namedWindow('RealSense')
+        cv2.imshow('RealSense', color_image)
+
+
+
+        # Break from loop if the "Q" key is pressed
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
 
 finally:
 
     # Stop streaming
+    print("[INFO] ending video stream...")
     pipeline.stop()
-'''
