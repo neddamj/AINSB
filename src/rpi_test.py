bd69dd11b201f35211d85a73d8e7aeea87df2445
Jordan-Madden <jordanmadden285@gmail.com>
2021-04-24 18:57:45 -0400

    Rename file and update depth filter

--- src/rpi_test.py
+++ src/rpi_test.py
@@ -4,7 +4,7 @@
 '''
 
 import tflite_runtime.interpreter as tflite
-import pyrealsense2.pyrealsense2 as rs
+from realsense import RealSense
 from imutils.video import FPS
 from threading import Thread
 from smbus import SMBus
@@ -29,56 +29,6 @@ args = vars(ap.parse_args())
 # Set the bus address and indicate I2C-1
 addr = 0x08
 bus = SMBus(1)
-
-class RealSenseVideo:
-    def __init__(self, width=640, height=480):
-        # Frame dimensions of camera
-        self.width = width
-        self.height = height
-
-        # Build and enable the depth and color frames
-        self.pipeline = rs.pipeline()
-        self.config = rs.config()
-        self.config.enable_stream(rs.stream.depth, self.width, self.height, rs.format.z16, 30)
-        self.config.enable_stream(rs.stream.color, self.width, self.height, rs.format.bgr8, 30)
-
-        # Start streaming
-        self.pipeline.start(self.config)
-        
-        # Read the first frame from the stream
-        self.frame = self.pipeline.wait_for_frames()
-        self.depth_frame = self.frame.get_depth_frame()
-        self.color_frame = self.frame.get_color_frame()
-
-        # Variable to check if thread should be stopped
-        self.stopped = False
-
-    def start(self):
-        # Start the thread to read the frames from the video stream
-        Thread(target=self.update, args=()).start()
-        return self
-
-    def update(self):
-        while True:
-            # Stop streaming in indicator is set
-            if self.stopped:
-                return
-
-            # Otherwise read the next frame in the stream
-            self.frame = self.pipeline.wait_for_frames()
-            self.depth_frame = self.frame.get_depth_frame()
-            self.color_frame = self.frame.get_color_frame()
-            if not self.depth_frame or not self.color_frame:
-                return
-
-    def read(self):
-        # Return the most recent color and depth frame
-        return self.color_frame, self.depth_frame
-
-    def stop(self)        :
-        # Stop the video stream
-        self.stopped = True
-        self.pipeline.stop()
 
 def detect(input_data, input_details, output_details):
     # Perform the object detection and get the results
@@ -109,7 +59,7 @@ def filter_distance(depth_frame, x, y):
     positive = np.random.randint(low=30, high=100)
 
     i = 0
-    while(i < 75):
+    while(i < 50):
         # Extract the depth value from the camera
         dist = int(depth_frame.get_distance(x, y) * 100)
         
@@ -287,7 +237,7 @@ if __name__ == "__main__":
 
     # Initialize video stream and the FPS counter
     print('[INFO] running inference for realsense camera...')
-    video = RealSenseVideo(width=imW, height=imH).start()
+    video = RealSense(width=imW, height=imH).start()
     fps = FPS().start()
     time.sleep(1)
 
