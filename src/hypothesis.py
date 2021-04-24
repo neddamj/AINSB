c7ea4a4d273275897b4e503b05e00ebd0e27d947
Jordan-Madden <jordanmadden285@gmail.com>
2021-04-24 18:54:58 -0400

    Testing depth profiles

---
+++ src/hypothesis.py
@@ -0,0 +1,84 @@
+'''
+    Author: Jordan Madden
+    Usage: python hypothesis.py
+'''
+
+import matplotlib.pyplot as plt
+from realsense import RealSense
+from imutils.video import FPS
+import numpy as np
+import time
+import cv2
+
+def filter_distance(depth_frame, x, y):
+    #List to store the consecutive distance values and randomly initialized variable
+    distances = []
+    positive = np.random.randint(low=30, high=100)
+
+    i = 0
+    while(i < 50):
+        # Extract the depth value from the camera
+        dist = int(depth_frame.get_distance(x, y) * 100)
+        
+        # Store the last positive value for use in the event that the
+        # value returned is 0
+        if dist != 0:
+            positive = dist
+        elif dist == 0:
+            positive == positive
+
+        # Add the distances to the list
+        distances.append(positive)
+        i += 1
+
+    # Convert the list to a numpy array and return it
+    distances = np.asarray(distances)
+    return int(distances.mean())
+
+if __name__ == "__main__":
+    video = RealSense(width=640, height=480).start()
+    fps = FPS().start()
+    
+    while True:
+        color_frame, depth_frame = video.read()
+        
+        # Convert images to numpy arrays and get the frame dimensions
+        depth_image = np.asanyarray(depth_frame.get_data())
+        frame = np.asanyarray(color_frame.get_data())
+        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Draw line on screen to visulalize 300px
+        cv2.line(frame, (100, 240), (600, 240), (0, 160, 200), 2)
+        #depth = depth_image[100:500,240:240].astype(float)*depth_scale
+        pts = []
+        
+        for i in range(500):
+            pts.append(filter_distance(depth_frame, 100+i, 240))
+        
+        print(pts)
+        plt.plot(pts)
+        plt.show()
+        
+        # All the results have been drawn on the frame, so it's time to display it.
+        cv2.imshow('Object Detector', frame)
+        fps.update()
+        
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            fps.stop()
+            break
+        
+    # Show the elapsed time and the respective fps
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approximate fps: {:.2f}".format(fps.fps()))
+    
+    # Stop the video stream
+    cv2.destroyAllWindows()
+    video.stop() 
+        
+        
+        
+        
+        
+        
---
+++ src/realsense.py
@@ -0,0 +1,59 @@
+'''
+    Author: Jordan Madden
+    Desccription: Allows the efficient use of the Intel Realsense D415 camera by reading the
+                  data from the camera on its own thread
+            
+'''
+
+import pyrealsense2.pyrealsense2 as rs
+from threading import Thread
+
+class RealSense:
+    def __init__(self, width=640, height=480):
+        # Frame dimensions of camera
+        self.width = width
+        self.height = height
+
+        # Build and enable the depth and color frames
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.depth, self.width, self.height, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.color, self.width, self.height, rs.format.bgr8, 30)
+
+        # Start streaming
+        self.pipeline.start(self.config)
+        
+        # Read the first frame from the stream
+        self.frame = self.pipeline.wait_for_frames()
+        self.depth_frame = self.frame.get_depth_frame()
+        self.color_frame = self.frame.get_color_frame()
+
+        # Variable to check if thread should be stopped
+        self.stopped = False
+
+    def start(self):
+        # Start the thread to read the frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            # Stop streaming in indicator is set
+            if self.stopped:
+                return
+
+            # Otherwise read the next frame in the stream
+            self.frame = self.pipeline.wait_for_frames()
+            self.depth_frame = self.frame.get_depth_frame()
+            self.color_frame = self.frame.get_color_frame()
+            if not self.depth_frame or not self.color_frame:
+                return
+
+    def read(self):
+        # Return the most recent color and depth frame
+        return self.color_frame, self.depth_frame
+
+    def stop(self)        :
+        # Stop the video stream
+        self.stopped = True
+        self.pipeline.stop()
