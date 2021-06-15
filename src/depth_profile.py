7264e6ef62f36fa3517ef83e7049c31aaa63053c
Jordan-Madden <jordanmadden285@gmail.com>
2021-06-15 11:55:15 -0500

    Moved filter_distance to realsense.py

--- src/depth_profile.py
+++ src/depth_profile.py
@@ -3,37 +3,12 @@
     Usage: python depth_profile.py
 '''
 
+from realsense import RealSense, filter_distance
 import matplotlib.pyplot as plt
-from realsense import RealSense
 from imutils.video import FPS
 import numpy as np
 import time
 import cv2
-
-def filter_distance(depth_frame, x, y):
-    #List to store the consecutive distance values and randomly initialized variable
-    distances = []
-    positive = np.random.randint(low=30, high=100)
-
-    i = 0
-    while(i < 50):
-        # Extract the depth value from the camera
-        dist = int(depth_frame.get_distance(x, y) * 100)
-        
-        # Store the last positive value for use in the event that the
-        # value returned is 0
-        if dist != 0:
-            positive = dist
-        elif dist == 0:
-            positive == positive
-
-        # Add the distances to the list
-        distances.append(positive)
-        i += 1
-
-    # Convert the list to a numpy array and return it
-    distances = np.asarray(distances)
-    return int(distances.mean())
 
 def get_depth_profile(depth_frame, profile_width, X, Y):
     # Ensure the coordiantes fall within the frame
